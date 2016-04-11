36be72a3f685583de24a5c933bed87935c5791b8
zollenz <dejean75@gmail.com>
2016-04-11 23:46:54 +0200

    Manual merge of changes to utils from euclid

--- src/dk/miosis/luxetemplate/utility/MiosisUtilities.hx
+++ src/dk/miosis/luxetemplate/utility/MiosisUtilities.hx
@@ -4,13 +4,31 @@ import luxe.Log.*;
 
 class MiosisUtilities
 {
-	public static function clear(arr:Array<Dynamic>)
-	{
+    public static function clear(arr:Array<Dynamic>)
+    {
         #if (cpp || php)
         arr.splice(0, arr.length);
         #else
         untyped arr.length = 0;
         #end
+    }
+
+    public static function bitwise_right_circular_shift(value:Int, shift:Int, bit_count:Int):Int
+    {
+        assert(bit_count <= 64, 'The bit count (${bit_count}) can not be higher than 64');
+        assert(shift < bit_count, 'shift (${shift}) can not be higher than the bit count (${bit_count})');
+
+        // TODO: Replace conversions and string mangling with bitwise ops
+        var result_string = bitmask_int_to_string(value, bit_count);
+        var split_index = result_string.length - shift;
+        var overflow_string = result_string.substr(0, split_index);
+
+        result_string = result_string.substr(split_index);
+        result_string += overflow_string;
+
+        log(result_string);
+
+        return bitmask_string_to_int(result_string);
     }
 
     public static function bitmask_int_to_string(value:Int, size:Int = 64, spaced:Bool = false):String
@@ -41,16 +59,18 @@ class MiosisUtilities
     public static function bitmask_string_to_int(str:String):Int
     {
         var value = 0;
-        var i = str.length;
+        var i = 0;
 
-        while (--i > -1)
+        while (i < str.length)
         {
             if (str.charAt(i) == "1")
-            {
-                value |= (1 << i);              
+            {               
+                value = value | (1 << (str.length - i - 1));              
             }
+
+            ++i;
         }
 
         return value;
-    }	
+    }
 }
